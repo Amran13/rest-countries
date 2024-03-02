627cb8d8fffd1d2c655d95ea2d8a21e333fb1e60
amranhossain <amranhossainchy@gmail.com>
2024-03-02 23:54:19 +0600

    done with success

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,29 +4,32 @@ import { authContext } from '../Provider/AuthProvider';
 
 const Header = () => {
     const contextInfo = useContext(authContext)
-    const {user, logOut} = contextInfo
+    const { user, logOut } = contextInfo
     console.log(user)
     const handleLogOut = () => {
         logOut()
-        .then(res => console.log('log out successfully'))
-        .catch(err => console.error)
+            .then(res => console.log('log out successfully'))
+            .catch(err => console.error)
     }
 
     return (
-        <div>
+        <div className='flex justify-center'>
             <Link className='mx-2' to="/">Home</Link>
             {
                 user && <Link className='mx-2' to="/countries">Countries</Link>
             }
             <Link className='mx-2' to="/login">Login</Link>
             <Link className='mx-2' to="/register">Register</Link>
-            
-            {
-                user ? <div>
-                    <Link className='mx-2' to="/dashboard">Dashboard</Link>
-                <button onClick={handleLogOut}>LogOut</button>
-                </div> : ''
-            }
+
+            <div>
+                {
+                    user ? <div className='flex '>
+                        <Link className='mx-2' to="/dashboard">Dashboard</Link>
+                        <p className='mx-8'> {user.email} </p>
+                        <button onClick={handleLogOut}>LogOut</button>
+                    </div> : ''
+                }
+            </div>
         </div>
     );
 };
